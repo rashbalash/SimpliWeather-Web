cd58a67e5029da7d871c0b42ebe2047519904ec7
Rashad Balashov <Rashad.Balashov@gmail.com>
2024-11-28 06:50:34 -0500

    Added the initial design of the hourly weather card

--- simpliweather/src/App.js
+++ simpliweather/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HourlyWeatherCard from './web/dashboard/components/hourlyWeatherCard';
 
 function App() {
   return (
     <div className="App">
       <header className="main-header">
         <p>SimpliWeather</p>
+        <HourlyWeatherCard condition={"partly cloudy"} hour={"9 pm"} temperature={"52"} />
       </header>
     </div>
   );
---
+++ simpliweather/src/web/dashboard/components/hourlyWeatherCard.js
@@ -0,0 +1,19 @@
+import './css/hourlyWeatherCard.css';
+
+const HourlyWeatherCard = ({ hour, condition, temperature }) => {
+  return (
+    <div className="hourly-weather-card">
+      <p className="hourly-weather-hour">{hour}</p>
+      <div className="hourly-weather-icon">
+        {/* Replace the <div> below with an actual weather icon SVG or image */}
+        <div className="icon sun-cloud">
+          <span className="sun"></span>
+        </div>
+      </div>
+      <p className="hourly-weather-condition">{condition}</p>
+      <p className="hourly-weather-temp">{temperature}°</p>
+    </div>
+  );
+};
+
+export default HourlyWeatherCard;
